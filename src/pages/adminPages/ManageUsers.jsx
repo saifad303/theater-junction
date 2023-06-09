282c227e52d9911aa17acfe4c730c455b9fa449c
Saif <saifad303@gmail.com>
2023-06-09 14:12:11 +0600

    built manage user table responsive.

--- src/pages/adminPages/ManageUsers.jsx
+++ src/pages/adminPages/ManageUsers.jsx
@@ -1,53 +1,112 @@
 import React from "react";
 
 const ManageUsers = () => {
+  const tableItems = [
+    {
+      avatar:
+        "https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ",
+      name: "Liam James",
+      email: "liamjames@example.com",
+      phone_nimber: "+1 (555) 000-000",
+      position: "Software engineer",
+      salary: "$100K",
+    },
+    {
+      avatar: "https://randomuser.me/api/portraits/men/86.jpg",
+      name: "Olivia Emma",
+      email: "oliviaemma@example.com",
+      phone_nimber: "+1 (555) 000-000",
+      position: "Product designer",
+      salary: "$90K",
+    },
+    {
+      avatar: "https://randomuser.me/api/portraits/women/79.jpg",
+      name: "William Benjamin",
+      email: "william.benjamin@example.com",
+      phone_nimber: "+1 (555) 000-000",
+      position: "Front-end developer",
+      salary: "$80K",
+    },
+    {
+      avatar: "https://api.uifaces.co/our-content/donated/xZ4wg2Xj.jpg",
+      name: "Henry Theodore",
+      email: "henrytheodore@example.com",
+      phone_nimber: "+1 (555) 000-000",
+      position: "Laravel engineer",
+      salary: "$120K",
+    },
+    {
+      avatar:
+        "https://images.unsplash.com/photo-1439911767590-c724b615299d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ",
+      name: "Amelia Elijah",
+      email: "amelia.elijah@example.com",
+      phone_nimber: "+1 (555) 000-000",
+      position: "Open source manager",
+      salary: "$75K",
+    },
+  ];
+
   return (
     <div className="p-4 sm:ml-64">
       <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg  mt-14">
-        <div>ManageUsers</div>
-        <div className="grid grid-cols-3 gap-4 mb-4">
-          <div className="flex items-center justify-center h-24 rounded bg-gray-50 ">
-            <p className="text-2xl text-gray-400 ">+</p>
-          </div>
-          <div className="flex items-center justify-center h-24 rounded bg-gray-50 ">
-            <p className="text-2xl text-gray-400 ">+</p>
-          </div>
-          <div className="flex items-center justify-center h-24 rounded bg-gray-50 ">
-            <p className="text-2xl text-gray-400 ">+</p>
-          </div>
-        </div>
-        <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 ">
-          <p className="text-2xl text-gray-400 ">+</p>
-        </div>
-        <div className="grid grid-cols-2 gap-4 mb-4">
-          <div className="flex items-center justify-center rounded bg-gray-50 h-28 ">
-            <p className="text-2xl text-gray-400 ">+</p>
-          </div>
-          <div className="flex items-center justify-center rounded bg-gray-50 h-28 ">
-            <p className="text-2xl text-gray-400 ">+</p>
-          </div>
-          <div className="flex items-center justify-center rounded bg-gray-50 h-28 ">
-            <p className="text-2xl text-gray-400 ">+</p>
-          </div>
-          <div className="flex items-center justify-center rounded bg-gray-50 h-28 ">
-            <p className="text-2xl text-gray-400 ">+</p>
-          </div>
-        </div>
-        <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 ">
-          <p className="text-2xl text-gray-400 ">+</p>
-        </div>
-        <div className="grid grid-cols-2 gap-4">
-          <div className="flex items-center justify-center rounded bg-gray-50 h-28 ">
-            <p className="text-2xl text-gray-400 ">+</p>
-          </div>
-          <div className="flex items-center justify-center rounded bg-gray-50 h-28 ">
-            <p className="text-2xl text-gray-400 ">+</p>
-          </div>
-          <div className="flex items-center justify-center rounded bg-gray-50 h-28 ">
-            <p className="text-2xl text-gray-400 ">+</p>
-          </div>
-          <div className="flex items-center justify-center rounded bg-gray-50 h-28 ">
-            <p className="text-2xl text-gray-400 ">+</p>
+        <div className="flex items-center justify-center mb-4 rounded bg-gray-50 p-10">
+          <div className="w-full mx-auto px-4 md:px-8">
+            <div className="items-start justify-between md:flex">
+              <div className="max-w-lg">
+                <h3 className="text-gray-800 text-xl font-bold sm:text-2xl">
+                  All Users
+                </h3>
+              </div>
+            </div>
+            <div className="mt-12 shadow-sm border rounded-lg overflow-x-auto">
+              <table className="w-full table-auto text-sm text-left">
+                <thead className="bg-gray-50 text-gray-600 font-medium border-b">
+                  <tr>
+                    <th className="py-3 px-6">Username</th>
+                    <th className="py-3 px-6">Email</th>
+                    <th className="py-3 px-6">Role</th>
+                    <th className="py-3 px-6">Student / Instructor</th>
+                  </tr>
+                </thead>
+                <tbody className="text-gray-600 divide-y">
+                  {tableItems.map((item, idx) => (
+                    <tr key={idx}>
+                      <td className="flex items-center gap-x-3 py-3 px-6 whitespace-nowrap">
+                        <img
+                          src={item.avatar}
+                          className="w-10 h-10 rounded-full"
+                        />
+                        <div>
+                          <span className="block text-gray-700 text-sm font-medium">
+                            {item.name}
+                          </span>
+                        </div>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        {item.email}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        {item.position}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <button
+                          href="javascript:void()"
+                          className="py-2 leading-none px-3 font-medium text-white rounded-lg bg-[#54C6C4]"
+                        >
+                          Make Student
+                        </button>
+                        <button
+                          href="javascript:void()"
+                          className="py-2 leading-none px-3 font-medium text-white rounded-lg bg-[#F89A2E]"
+                        >
+                          Make Instructor
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
